fc0850a32698027f4b5c78179c74a58432d2879b
NiKole Maxwell <nvymaxwell@outlook.com>
2017-10-15 18:28:32 -0400

    Initial commit - Original 7-13-2017

---
+++ app.js
@@ -0,0 +1,9 @@
+/*/         Inital State (page load)       /*/
+
+/*/         On Click (Press Play)        /*/
+
+/*/         Shuffle                            /*/
+
+
+
+/*/     Reshuffle (is this needed?)       /*/
